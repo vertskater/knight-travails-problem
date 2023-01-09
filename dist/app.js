c587d71b6c018dc7e2c22d37984281b73d6090ec
vertskater <vertskater@gmx.at>
2023-01-09 08:48:17 +0100

    find Path with BFS-Algorytm

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,71 @@
-import Board from "./modules/board.js";
+"use strict";
+/* import Board from "./modules/board.js";
 import { addMove, addAllPossibleMoves } from "./modules/knightMoves.js";
+
 new Board();
-const findPath = (startXY, endXY) => {
-    let [startX, startY] = startXY;
-    let [endX, endY] = endXY;
-    addMove(startX, startY, 0);
-    let index = 0;
-    do {
-        addAllPossibleMoves(index++);
-    } while (Board.board[endX][endY] === undefined);
-    return Board.board[endX][endY];
+
+const findPath = (startXY: [number, number], endXY: [number, number]) => {
+  let [startX, startY] = startXY;
+  let [endX, endY] = endXY;
+  addMove(startX, startY, 0);
+  let index = 0;
+  do {
+    addAllPossibleMoves(index++);
+  } while (Board.board[endX][endY] === undefined);
+  return Board.board[endX][endY];
 };
-console.log(findPath([3, 3], [4, 3]));
+
+console.log(findPath([3, 3], [4, 3])); */
+class Cell {
+    constructor(x, y, distance) {
+        this.x = x;
+        this.y = y;
+        this.distance = distance;
+    }
+}
+const isInside = (x, y, distance) => {
+    if (x > 0 && x <= distance && y > 0 && y <= distance) {
+        return true;
+    }
+    return false;
+};
+const cellUnvisited = (size) => {
+    const visit = new Array(size);
+    for (let i = 0; i < size; i++) {
+        visit[i] = new Array(size);
+        for (let j = 0; j < size; j++) {
+            visit[i][j] = false;
+        }
+    }
+    return visit;
+};
+function minStepsToReachTarget(startingPoint, endPoint, size) {
+    const [startX, startY] = startingPoint;
+    const [endX, endY] = endPoint;
+    let dx = [-2, -1, 1, 2, -2, -1, 1, 2];
+    let dy = [-1, -2, -2, -1, 1, 2, 2, 1];
+    const knightStateQueue = [];
+    knightStateQueue.push(new Cell(startX, startY, 0));
+    let x, y;
+    let currentElementInQueue;
+    const visit = cellUnvisited(size);
+    visit[startX][startY] = true;
+    console.log(visit, knightStateQueue);
+    while (knightStateQueue.length !== 0) {
+        currentElementInQueue = knightStateQueue.shift();
+        if (currentElementInQueue.x === endX && currentElementInQueue.y === endY) {
+            return currentElementInQueue.distance;
+        }
+        for (let i = 0; i < 8; i++) {
+            x = currentElementInQueue.x + dx[i];
+            y = currentElementInQueue.y + dy[i];
+            if (isInside(x, y, size) && !visit[x][y]) {
+                visit[x][y] = true;
+                knightStateQueue.push(new Cell(x, y, currentElementInQueue.distance + 1));
+            }
+        }
+    }
+    return Number.MAX_VALUE;
+}
+console.log(minStepsToReachTarget([3, 3], [8, 8], 8));
 //# sourceMappingURL=app.js.map
